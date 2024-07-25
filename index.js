d8949ebce709a939cc2c67ad2188fc6b65523d9a
Patrick <njorogepatrick739@gmail.com>
2024-07-26 00:15:32 +0300

    submit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function moveDodgerLeft() {
+  const dodger = document.getElementById("dodger");
+  const left = parseInt(dodger.style.left, 10);
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const dodger = document.getElementById("dodger");
+  const left = parseInt(dodger.style.left, 10);
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
